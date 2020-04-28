50d85608bc5dd455c46f29a16e43cfda45d51634
antonbelous <belous.nsk@gmail.com>
2020-04-28 21:23:56 +0300

    Add logging

--- src/main/java/ru/antonbelous/eventmanagement/web/UserServlet.java
+++ src/main/java/ru/antonbelous/eventmanagement/web/UserServlet.java
@@ -1,5 +1,8 @@
 package ru.antonbelous.eventmanagement.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +11,13 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
 
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        log.debug("redirect to users");
+
+        //request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");
     }
 }
