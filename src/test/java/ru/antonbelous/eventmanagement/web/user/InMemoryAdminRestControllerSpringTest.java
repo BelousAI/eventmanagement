d3a7754a6348888c8562fdd071d6b937d5feca74
antonbelous <belous.nsk@gmail.com>
2020-05-06 21:08:29 +0300

    Add spring-test

---
+++ src/test/java/ru/antonbelous/eventmanagement/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,44 @@
+package ru.antonbelous.eventmanagement.web.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.antonbelous.eventmanagement.UserTestData;
+import ru.antonbelous.eventmanagement.inmemory.InMemoryUserRepository;
+import ru.antonbelous.eventmanagement.model.User;
+import ru.antonbelous.eventmanagement.util.exception.NotFoundException;
+
+import java.util.Collection;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository.init();
+    }
+
+    @Test
+    public void delete() throws Exception {
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(1, users.size());
+        Assert.assertEquals(UserTestData.ADMIN, users.iterator().next());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteNotFound() throws Exception {
+        controller.delete(10);
+    }
+}
